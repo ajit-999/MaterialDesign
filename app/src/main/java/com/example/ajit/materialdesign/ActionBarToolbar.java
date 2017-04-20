4d296c15f29fe591239e16cd8ba7ca5646720a8d
ajit-999 <ajitkumarjain123@gmail.com>
2017-04-21 01:00:32 +0530

    Three type of toolbar complete

--- app/src/main/java/com/example/ajit/materialdesign/ActionBarToolbar.java
+++ app/src/main/java/com/example/ajit/materialdesign/ActionBarToolbar.java
@@ -2,6 +2,10 @@ package com.example.ajit.materialdesign;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ActionBarToolbar extends AppCompatActivity {
 
@@ -9,5 +13,41 @@ public class ActionBarToolbar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toolbar);
+        Toolbar mToolbar =(Toolbar)findViewById(R.id.tool);
+        setSupportActionBar(mToolbar);
+
+        getSupportActionBar().setTitle("Action Bar Toolbar");
+        mToolbar.setSubtitle("By Shopnfest");
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String msg = "";
+        switch (item.getItemId()){
+            case R.id.save:
+                msg =getString(R.string.save);
+                break;
+            case R.id.mail:
+                msg =getString(R.string.mail);
+                break;
+            case R.id.setting:
+                msg = getString(R.string.setting);
+                break;
+            case R.id.help:
+                msg = getString(R.string.Help);
+                break;
+        }
+        Toast.makeText(this,""+msg,Toast.LENGTH_LONG).show();
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/ajit/materialdesign/StandAloneToolbar.java
+++ app/src/main/java/com/example/ajit/materialdesign/StandAloneToolbar.java
@@ -4,6 +4,8 @@ import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class StandAloneToolbar extends AppCompatActivity {
 
@@ -14,13 +16,32 @@ public class StandAloneToolbar extends AppCompatActivity {
         Toolbar mToolbar =(Toolbar)findViewById(R.id.tool);
         mToolbar.setTitle("StandAlone");
         mToolbar.setSubtitle("Shopnfest");
-        mToolbar.setNavigationIcon(R.drawable.navi);
+        mToolbar.setNavigationIcon(R.drawable.ic_action_navigation);
+        mToolbar.inflateMenu(R.menu.menu_main);
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP)
         {
 
             mToolbar.setElevation(10f);
 
+
         }
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                String title = item.getTitle().toString();
+                Toast.makeText(getApplicationContext(),""+title,Toast.LENGTH_LONG).show();
+                switch (item.getItemId()){
+                    case R.id.save:
+                        //Perform something
+                        break;
+                    case R.id.setting:
+                        //Perform Something
+                    case R.id.mail:
+                        break;
+                }
+                return true;
+            }
+        });
 
 
     }
